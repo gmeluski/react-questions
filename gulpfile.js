86131cb8986c808484bdb115bbe7216f0a5f1a15
Gregg Meluski <gmeluski@hotmail.com>
2015-07-23 14:48:43 -0400

    add in first gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,20 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var react = require('gulp-react');
 var htmlreplace = require('gulp-html-replace');
+
+var path = {
+  HTML: 'src/index.html',
+  ALL: [],
+  JS: ['src/js/*.js', 'src/js/**/*.js'],
+  MINIFIED_OUT: '',
+  DEST_SRC: 'dist/src',
+  DEST_BUILD: '',
+  DEST: ''
+};
+
+// change JSX to JS
+gulp.task('transform', function () {
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(gulp.dest(path.DEST_SRC));
+});
