9c232f529d111f435bc3b3d624c4baa9ec863dac
Gregg Meluski <gmeluski@hotmail.com>
2015-07-28 18:12:19 -0400

    add in the pieces, add in the build process

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var htmlreplace = require('gulp-html-replace');
 
 var path = {
   HTML: 'src/index.html',
-  ALL: [],
+  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
   JS: ['src/js/*.js', 'src/js/**/*.js'],
   MINIFIED_OUT: '',
   DEST_SRC: 'dist/src',
   DEST_BUILD: '',
-  DEST: ''
+  DEST: 'dist'
 };
 
 // change JSX to JS
@@ -20,3 +20,34 @@ gulp.task('transform', function () {
     .pipe(react())
     .pipe(gulp.dest(path.DEST_SRC));
 });
+
+// copy index to destination
+gulp.task('copy', function () {
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+// watch the paths and perform transform & copy
+gulp.task('watch', function () {
+  gulp.watch(path.ALL, ['transform', 'copy']);
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('build', function () {
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(concat(path.MINIFIED_OUT))
+    .pipe(uglify(path.MINIFIED_OUT))
+    .pipe(gulp.dest(path.DEST_BUILD));
+});
+
+gulp.task('replaceHTML', function () {
+  gulp.src(path.HTML)
+    .pipe(htmlreplace({
+      js: 'build/' + path.MINIFIED_OUT
+    }))
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('production', ['replaceHTML', 'build']);
